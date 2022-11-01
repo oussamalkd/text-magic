15910f159c651f1989dd6805ede8b1db2da7f1ab
Oussama Louelkadi <oussamalkd@pop-os.localdomain>
2022-11-01 14:17:04 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+
+</script>
+
+<template>
+  <div class="bg-blue-900 w-full h-screen">
+    <h1>Hello WOrrrld</h1>
+  </div>
+</template>
+
+<style scoped>
+#app {
+  @apply w-full h-screen
+}
+</style>
---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
